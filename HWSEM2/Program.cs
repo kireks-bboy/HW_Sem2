64b7f793aebc7e994ae2489ae7a9b2cdaa7d1c9f
Kirill Denisevitch <denviko00@gmail.com>
2022-11-26 12:30:50 +0300

    HW_Sem2

---
+++ HWSEM2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// Console.WriteLine("Vvedite trehznachnoe chislo");
+// int x = Convert.ToInt32(Console.ReadLine());
+// if (x < 1000 && x > 99)
+// {
+// int two = x / 10;
+// int result = two % 10;
+// Console.Write(result);
+// }
+// else
+// Console.WriteLine("Vi vveli ne trexznachnoe chislo");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+// Console.WriteLine("Vvedite trehznachnoe chislo");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// if (x < 100)
+//  {
+//     Console.Write("Tretie cifri net");
+//  }
+// else
+// {
+// while (x >= 1000)
+// {
+//     x = x / 10;
+// }
+
+// if (x < 1000)
+//  { 
+//     int three = x % 100;
+//     int tow = three % 10;
+//     Console.Write(tow);
+//  }
+//  }
+
+
+
+//  Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("\n Vvedite nomer dnya nedeli  "); 
+//                 int num = int.Parse(Console.ReadLine()); 
+//                 if (num >= 1 && num <= 5) 
+//                 { 
+//                     Console.Write(" Net eto ne vihodnoy"); 
+//                 } 
+ 
+//                 else if (num >= 6 && num <= 7) 
+//                 { 
+//                     Console.Write(" Da eto vihodnoy"); 
+//                 } 
+//                 else Console.Write(" Ne sycshestvuet takogo dnya nedeli"); 
+            
+        
+    
+
+
